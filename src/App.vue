b4bf411be0e4b1c587907916a262dba41ff1aa56
Douglas Cavalcante <38563983+douglas-cavalcante@users.noreply.github.com>
2023-08-10 21:00:46 -0300

    adiciona projeto inicial

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <Menu />
+
+  <router-view></router-view>
+  
+  RODAPE
+</template>
+
+
+<script>
+import Menu from './components/Menu/Menu.vue'
+
+export default {
+  components: {
+    Menu
+  }
+}
+</script>
---
+++ src/views/Talent/New/TalentNew.vue
@@ -0,0 +1,4 @@
+<template>
+    cadastro de talento
+</template>
+
